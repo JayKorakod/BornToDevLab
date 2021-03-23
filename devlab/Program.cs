9111b495fc5eb893ddc52c2d8e009b09814d976e
JayKorakod <korakod.kaisri.gmail.com>
2021-03-23 17:04:33 +0700

    first commit

---
+++ devlab/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region === ReverseThe ===
+            //string text = Console.ReadLine();
+            //string[] textArray = text.Split(' ');
+            //foreach(string txt in textArray.Reverse())
+            //{
+            //    Console.Write(txt + " ");
+            //}
+            #endregion
+            #region === น้ำหนักปลา ===
+            //DEVLAB น้ำหนักปลา
+            //int n;
+            //string select;
+            //string max = "MAX";
+            //string min = "MIN";
+            //List<int> number = new List<int>();
+
+            //do
+            //{
+            //    int.TryParse(Console.ReadLine(), out n);
+            //    number.Add(n);
+            //} while (n != 0);
+            //number.Remove(0);
+            //select = Console.ReadLine();
+            //number.Sort();
+            //if (string.Equals(select,min,StringComparison.OrdinalIgnoreCase))
+            //{
+            //    foreach (int i in number)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+            //else if (string.Equals(select,max,StringComparison.OrdinalIgnoreCase))
+            //{
+            //    number.Reverse();
+            //    foreach (int i in number)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
